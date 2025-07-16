d7d64e259423b05844abc1b9276aa9c00782f0f0
debasishrabha <157095206+debasishrabha@users.noreply.github.com>
2025-07-16 20:18:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,6 @@ import react from '@vitejs/plugin-react'
 
 // Replace 'your-repo-name' with your actual GitHub repo name
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/debasish/',
   plugins: [react()],
 })
